86b137576514cc24593e02c9e29f56cd0c2bdaf6
raunakpurohit <raunak.wscubetech@gmail.com>
2024-11-05 20:56:33 +0530

    added comments

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,9 +6,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      ///appbar
       appBar: AppBar(
         title: Text('Home'),
       ),
+      ///body
       body: Center(
         child: Text('Welcome'),
       ),
