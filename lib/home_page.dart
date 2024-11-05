8a40da607d26e85eba00c9208af0c8af406a19db
raunakpurohit <raunak.wscubetech@gmail.com>
2024-11-05 20:53:41 +0530

    added home ui

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Welcome'),
+      ),
+    );
   }
 }
